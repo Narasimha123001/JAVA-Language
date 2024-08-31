b05f589a653cc3b3aa1c619b8ad3e0702074f841
Narasimha <narasimhaanguluri001@gmail.com>
2024-08-31 12:46:47 +0530

    implementing the ascii character set in java

---
+++ JavaFundamentals/Day1WorkingWithPrimitives/Exercise/ASCIIChar.java
@@ -0,0 +1,8 @@
+package com.JavaFundamentals.Day1WorkingWithPrimitives.Exercise;
+
+public class ASCIIChar {
+    public static void main(String[] args) {
+        char letter =  'A';
+        System.out.println("ASCII value of 'A' :"+(int)letter);
+    }
+}
