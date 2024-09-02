70ed4ce8fddc1dded306779ff8db2956cbb37bb3
Narasimha <narasimhaanguluri001@gmail.com>
2024-09-02 14:11:29 +0530

    implementing the stwich Statement in java

--- JavaFundamentals/Day2CoditionalStatement/Exercise/SwitchCondition.java
+++ JavaFundamentals/Day2CoditionalStatement/Exercise/SwitchCondition.java
@@ -3,51 +3,53 @@ package com.JavaFundamentals.Day2CoditionalStatement.Exercise;
 public class SwitchCondition {
     /*
     brand:iQoo,Apple,Nokia.
-    Membership:Diamond,Gold,Silver,Brorzen.
+    Membership:Diamond,Gold,Silver,Bronze.
      */
     public static void main(String[] args) {
         String Brand;
-        Brand="Samsung";
+        Brand = "Samsung";
         int ProductPrize;
-        ProductPrize =300000;
+        ProductPrize = 300000;
         int finalPrize;
         int ProductDiscount;
-        char Membership = 'D';
+        char Membership = 'D'; // D for Diamond, G for Gold, S for Silver
         int Discount;
-        ProductDiscount =1;
-        
+        ProductDiscount = 1;
+
         switch (Brand) {
             case "Apple":
-                if (Membership == 'D'){
-                    ProductDiscount =20;
-                } else if(Membership == 'G'){
+                if (Membership == 'D') {
+                    ProductDiscount = 20;
+                } else if (Membership == 'G') {
                     ProductDiscount = 15;
-                } else if(Membership == 'S'){
+                } else if (Membership == 'S') {
                     ProductDiscount = 10;
                 } else {
                     ProductDiscount = 5;
                 }
                 break;
             case "Samsung":
-                if (Membership == 'D'){
-                    ProductDiscount =30;
-                } else if(Membership == 'G'){
+                if (Membership == 'D') {
+                    ProductDiscount = 30;
+                } else if (Membership == 'G') {
                     ProductDiscount = 55;
-                } else if(Membership == 'S'){
+                } else if (Membership == 'S') {
                     ProductDiscount = 12;
                 } else {
                     ProductDiscount = 25;
                 }
-                break;
+                break;  // Added break statement here to avoid falling through to default case
             default:
-                ProductDiscount =5;
-
+                ProductDiscount = 5;
+                break;
         }
+
         Discount = ProductPrize * ProductDiscount / 100;
         finalPrize = ProductPrize - Discount;
-        System.out.println("Brand: "+Brand);
-        System.out.println("ProductDiscount: "+ProductDiscount);
-        System.out.println("Discount: "+Discount);
-        System.out.println("FinalPrize: "+finalPrize);
+
+        System.out.println("Brand: " + Brand);
+        System.out.println("ProductDiscount: " + ProductDiscount);
+        System.out.println("Discount: " + Discount);
+        System.out.println("FinalPrize: " + finalPrize);
     }
 }
