00f00f9f19baa49c688af80d367d7f2c10907580
Narasimha <narasimhaanguluri001@gmail.com>
2024-09-02 14:14:17 +0530

    aome basic operations with operends

---
+++ JavaFundamentals/Day2CoditionalStatement/Exercise/OperatedTest.java
@@ -0,0 +1,13 @@
+package com.JavaFundamentals.Day2CoditionalStatement.Exercise;
+
+public class OperatedTest {
+    public static void main(String[] args) {
+        int Salary;
+        Salary =300000;
+
+        int IntrementSalary;
+        IntrementSalary = Salary + (Salary*10) / 100;
+
+        System.out.println("IntrementSalary: "+IntrementSalary);
+    }
+}
