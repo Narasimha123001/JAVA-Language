dfa3bb85da68fdaf5a151625f462cd5c8715a1af
Narasimha <narasimhaanguluri001@gmail.com>
2024-09-02 14:03:59 +0530

    implementing the switch statement in java

---
+++ JavaFundamentals/Day2CoditionalStatement/Exercise/SwitchCondition.java
@@ -0,0 +1,53 @@
+package com.JavaFundamentals.Day2CoditionalStatement.Exercise;
+
+public class SwitchCondition {
+    /*
+    brand:iQoo,Apple,Nokia.
+    Membership:Diamond,Gold,Silver,Brorzen.
+     */
+    public static void main(String[] args) {
+        String Brand;
+        Brand="Samsung";
+        int ProductPrize;
+        ProductPrize =300000;
+        int finalPrize;
+        int ProductDiscount;
+        char Membership = 'D';
+        int Discount;
+        ProductDiscount =1;
+        
+        switch (Brand) {
+            case "Apple":
+                if (Membership == 'D'){
+                    ProductDiscount =20;
+                } else if(Membership == 'G'){
+                    ProductDiscount = 15;
+                } else if(Membership == 'S'){
+                    ProductDiscount = 10;
+                } else {
+                    ProductDiscount = 5;
+                }
+                break;
+            case "Samsung":
+                if (Membership == 'D'){
+                    ProductDiscount =30;
+                } else if(Membership == 'G'){
+                    ProductDiscount = 55;
+                } else if(Membership == 'S'){
+                    ProductDiscount = 12;
+                } else {
+                    ProductDiscount = 25;
+                }
+                break;
+            default:
+                ProductDiscount =5;
+
+        }
+        Discount = ProductPrize * ProductDiscount / 100;
+        finalPrize = ProductPrize - Discount;
+        System.out.println("Brand: "+Brand);
+        System.out.println("ProductDiscount: "+ProductDiscount);
+        System.out.println("Discount: "+Discount);
+        System.out.println("FinalPrize: "+finalPrize);
+    }
+}
