5d32b9a25bf1dbed2a1d2e476ab2bd963e30dd2a
Narasimha <narasimhaanguluri001@gmail.com>
2024-09-04 23:23:40 +0530

    sum of the even number in given range

---
+++ JavaFundamentals/DAY3CodingQuestions/Question/SumOfEven.java
@@ -0,0 +1,21 @@
+package com.JavaFundamentals.DAY3CodingQuestions.Question;
+
+import java.util.Scanner;
+
+public class SumOfEven {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a the range: ");
+        int num = scanner.nextInt();
+
+        int sum = 0;
+
+        for(int i=0;i<=num;i++){
+            if( i % 2 ==0){
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+        scanner.close();
+    }
+}
