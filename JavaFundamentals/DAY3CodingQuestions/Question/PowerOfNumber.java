d51ab5000e5bd7b2575675cfefb3ece97f6bd624
Narasimha <narasimhaanguluri001@gmail.com>
2024-09-04 23:19:36 +0530

    implementing the power of a digit in java

---
+++ JavaFundamentals/DAY3CodingQuestions/Question/PowerOfNumber.java
@@ -0,0 +1,27 @@
+package com.JavaFundamentals.DAY3CodingQuestions.Question;
+
+import java.util.Scanner;
+
+public class PowerOfNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter the base number: ");
+        int base = scanner.nextInt();
+
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Enter a power digit: ");
+        int power = scanner1.nextInt();
+
+        int ans = 1;
+
+        if(base == 0 || power == 0){
+            System.out.println(1);
+        }else{
+            for(int i= 1; i <= power;i++){
+                ans *= base;
+            }
+            System.out.println(ans);
+        }
+        scanner.close();
+    }
+}
