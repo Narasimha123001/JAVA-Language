b48ad43ef24b4dc06d2d17e5cade956e6cba99b3
Narasimha <narasimhaanguluri001@gmail.com>
2024-09-04 23:21:07 +0530

    checking the number is prime or not

---
+++ JavaFundamentals/DAY3CodingQuestions/Question/PrimeNumber.java
@@ -0,0 +1,23 @@
+package com.JavaFundamentals.DAY3CodingQuestions.Question;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = scanner.nextInt();
+        int FactorCount = 0;
+        for (int i = 1; i <= number ; i++){
+            if (number % i == 0){
+                FactorCount++;
+            }
+        }
+        if (FactorCount == 2){
+            System.out.println(number+" is a prime number");
+        }else{
+            System.out.println(number+" is not a prime nummber");
+        }
+        scanner.close();
+    }
+}
