c5a674d18fc8d3e2ca9141ebf3d3900ef453e157
Narasimha <narasimhaanguluri001@gmail.com>
2024-09-07 21:43:15 +0530

    simple exectution of class and object by oops with instance variables

---
+++ JavaFundamentals/OOPS/ClassAndObject/Exercise/Student.java
@@ -0,0 +1,18 @@
+package com.JavaFundamentals.OOPS.ClassAndObject.Exercise;
+
+public class Student {
+
+    int id=25;
+    String name ;
+    String Gender = "Male";
+
+
+    public static void main(String[] args) {
+
+
+        Student S1 = new Student();
+        System.out.println(S1.id);
+        System.out.println(S1.name);
+        System.out.println(S1.Gender);
+    }
+}
