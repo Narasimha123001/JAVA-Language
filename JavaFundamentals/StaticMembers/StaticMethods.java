824d1feb55290f04b93149219df81a6f5a4b81cf
Narasimha <narasimhaanguluri001@gmail.com>
2024-09-11 23:09:43 +0530

    display and print the sum of two variable using static method

---
+++ JavaFundamentals/StaticMembers/StaticMethods.java
@@ -0,0 +1,19 @@
+package com.JavaFundamentals.StaticMembers;
+
+public class StaticMethods {
+    //static method
+   public static int add(int a ,int b){
+       return a+b;
+   }
+
+    public static void main(String[] args) {
+        int result = StaticMethods.add(5,7);
+        System.out.println("result: "+result);
+    }
+
+}
+/*
+Static methods belongs to the class rather than instance of the class.
+ they can call without instance of the class .Static methods can only
+ access static variables and other static methods directly.
+ */
