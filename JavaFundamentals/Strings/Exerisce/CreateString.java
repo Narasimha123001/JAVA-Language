1b77b6148455a9ba1f5964027c4a4d276fb83323
Narasimha <narasimhaanguluri001@gmail.com>
2024-09-12 12:10:08 +0530

    creating a string with literals and new keyword

---
+++ JavaFundamentals/Strings/Exerisce/CreateString.java
@@ -0,0 +1,13 @@
+package com.JavaFundamentals.Strings.Exerisce;
+
+public class CreateString {
+    public static void main(String[] args) {
+        // using literals
+        String str1 = "Hello";
+        System.out.println("The string creations using Strings literals :" + str1);
+
+        //using new keyword
+        String str2 = new String("Hello World !");
+        System.out.println("the string creation using new Keyword : "+ str2);
+    }
+}
