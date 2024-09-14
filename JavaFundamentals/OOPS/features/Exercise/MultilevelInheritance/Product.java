0d07754fd17c2f7cccafe39679ed3ce7f79f9fcc
Narasimha <narasimhaanguluri001@gmail.com>
2024-09-14 23:40:16 +0530

    Parent class

---
+++ JavaFundamentals/OOPS/features/Exercise/MultilevelInheritance/Product.java
@@ -0,0 +1,8 @@
+package com.JavaFundamentals.OOPS.features.Exercise.MultilevelInheritance;
+
+public class Product {
+
+    void displayInfo(){
+        System.out.println("this is product");
+    }
+}
