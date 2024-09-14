ebc22c00f051f4858a58e8c6933462b9b950b8f8
Narasimha <narasimhaanguluri001@gmail.com>
2024-09-14 23:45:42 +0530

    its child class inherits from Product parent class

---
+++ JavaFundamentals/OOPS/features/Exercise/MultilevelInheritance/Laptop.java
@@ -0,0 +1,9 @@
+package com.JavaFundamentals.OOPS.features.Exercise.MultilevelInheritance;
+
+public class Laptop extends Product {
+
+    @Override
+    void displayInfo() {
+        System.out.println("this is Laptop");
+    }
+}
