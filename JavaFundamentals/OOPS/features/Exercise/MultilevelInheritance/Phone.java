7e4c671bc4228f835efa0e302ee2f1d93c3aaec6
Narasimha <narasimhaanguluri001@gmail.com>
2024-09-14 23:47:13 +0530

    its another child class inherits from Laptop

---
+++ JavaFundamentals/OOPS/features/Exercise/MultilevelInheritance/Phone.java
@@ -0,0 +1,11 @@
+package com.JavaFundamentals.OOPS.features.Exercise.MultilevelInheritance;
+
+
+
+public class Phone extends Laptop{
+
+    @Override
+    void displayInfo() {
+        System.out.println("this is phone");
+    }
+}
