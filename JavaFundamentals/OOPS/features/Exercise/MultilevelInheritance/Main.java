efe747b788e5b20ccbf99d79b559fdccfeb0192e
Narasimha <narasimhaanguluri001@gmail.com>
2024-09-14 23:49:06 +0530

    implemting of mutlilevel inheritance

---
+++ JavaFundamentals/OOPS/features/Exercise/MultilevelInheritance/Main.java
@@ -0,0 +1,8 @@
+package com.JavaFundamentals.OOPS.features.Exercise.MultilevelInheritance;
+
+public class Main {
+    public static void main(String[] args) {
+        Product product = new Phone();
+        product.displayInfo();
+    }
+}
