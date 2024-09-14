3be2d645ed73d8324d8ae03558ac5262c118884c
Narasimha <narasimhaanguluri001@gmail.com>
2024-09-14 23:43:48 +0530

    Parent class of this file having method to display details

--- JavaFundamentals/OOPS/features/Exercise/MultilevelInheritance/Product.java
+++ JavaFundamentals/OOPS/features/Exercise/MultilevelInheritance/Product.java
@@ -1,7 +1,7 @@
 package com.JavaFundamentals.OOPS.features.Exercise.MultilevelInheritance;
 
 public class Product {
-
+        //method
     void displayInfo(){
         System.out.println("this is product");
     }
