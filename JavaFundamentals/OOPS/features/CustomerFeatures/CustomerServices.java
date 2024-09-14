605cbf5e111efac1826897dcae78f739cdb1fa35
Narasimha <narasimhaanguluri001@gmail.com>
2024-09-14 23:07:12 +0530

    customer services file was service of all types of customer problem or requirments

---
+++ JavaFundamentals/OOPS/features/CustomerFeatures/CustomerServices.java
@@ -0,0 +1,10 @@
+package com.JavaFundamentals.OOPS.features.CustomerFeatures;
+
+public class CustomerServices {
+    // business logic for service
+
+    public void displayCustomerDetails(Customers customers){
+
+        customers.displayCustomerDetails();
+    }
+}
