e34cb672554b70ea2d0de543bef2a2ebba241dfe
Narasimha <narasimhaanguluri001@gmail.com>
2024-09-15 22:14:24 +0530

    product class is a parent class having product details and some display method

---
+++ JavaFundamentals/OOPS/features/Exercise/Hiechericallnheritance/Product.java
@@ -0,0 +1,8 @@
+package com.JavaFundamentals.OOPS.features.Exercise.Hiechericallnheritance;
+
+public class Product {
+
+    void displayInfo(){
+        System.out.println("this is product");
+    }
+}
