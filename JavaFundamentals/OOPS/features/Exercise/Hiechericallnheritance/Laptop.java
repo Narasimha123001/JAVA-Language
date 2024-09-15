fc6d31524682067b992f72b4ddd46e929fd26cb0
Narasimha <narasimhaanguluri001@gmail.com>
2024-09-15 22:16:22 +0530

    laptop file is child of product having the laptop details and dispay methods

---
+++ JavaFundamentals/OOPS/features/Exercise/Hiechericallnheritance/Laptop.java
@@ -0,0 +1,8 @@
+package com.JavaFundamentals.OOPS.features.Exercise.Hiechericallnheritance;
+
+public class Laptop extends Product{
+    @Override
+    void displayInfo() {
+        System.out.println("this is Laptop.");
+    }
+}
