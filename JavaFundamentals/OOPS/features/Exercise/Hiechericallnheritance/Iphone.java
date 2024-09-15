3052dd2b26f5cc6da06e3d3c0b95f06a3308654b
Narasimha <narasimhaanguluri001@gmail.com>
2024-09-15 22:17:45 +0530

    this is also a child class  of product having the same menthod

---
+++ JavaFundamentals/OOPS/features/Exercise/Hiechericallnheritance/Iphone.java
@@ -0,0 +1,9 @@
+package com.JavaFundamentals.OOPS.features.Exercise.Hiechericallnheritance;
+
+public class Iphone extends Product {
+
+    @Override
+    void displayInfo() {
+        System.out.println("this is Iphone");
+    }
+}
