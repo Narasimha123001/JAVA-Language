2a26d9d9271e49125c61e22a0ab75aeb3cef3df2
Narasimha <narasimhaanguluri001@gmail.com>
2024-09-16 22:22:08 +0530

    Book contains book title and author variables and display of them

---
+++ JavaFundamentals/OOPS/features/Task/LibrarySystem/Book.java
@@ -0,0 +1,17 @@
+package com.JavaFundamentals.OOPS.features.Task.LibrarySystem;
+
+public class Book {
+
+    String title;
+    String author;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+    void displayInfo(){
+        System.out.println("Book Details: ");
+        System.out.println("Title of the book: "+title);
+        System.out.println("Author of the book: "+author);
+    }
+}
