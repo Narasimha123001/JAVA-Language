0f48a710ea8a6f280058f4122a711f23ac065982
Narasimha <narasimhaanguluri001@gmail.com>
2024-09-28 15:16:30 +0530

    its was a abstract class having the abstract methods

---
+++ JavaFundamentals/OOPS/features/Abstractions/Exercise/Repository.java
@@ -0,0 +1,9 @@
+package com.JavaFundamentals.OOPS.features.Abstractions.Exercise;
+
+public abstract class Repository {
+
+    abstract void connectToDb();
+
+    abstract void addStudentToDb(Student student);
+
+}
