a082a026c8393230ce81b4ddd074c86347316694
Narasimha <narasimhaanguluri001@gmail.com>
2024-10-01 00:32:24 +0530

    implementation of default methods and abstract method

---
+++ JavaFundamentals/OOPS/features/Abstractions/Exercise2/Intefaces/Main1.java
@@ -0,0 +1,11 @@
+package com.JavaFundamentals.OOPS.features.Abstractions.Exercise2.Intefaces;
+
+public class Main1 {
+    public static void main(String[] args) {
+
+        Rectangle1 rectangle1 = new Rectangle1();
+
+        rectangle1.draw();
+        rectangle1.message();
+    }
+}
