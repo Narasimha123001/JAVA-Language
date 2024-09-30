c29576432b01e0e8c8d3fc11dbca10dc0f5d38b0
Narasimha <narasimhaanguluri001@gmail.com>
2024-10-01 01:01:23 +0530

    to test functionalites of to sub class and impplenting them

---
+++ JavaFundamentals/OOPS/features/Abstractions/Task/AnimalClassificationSystem/AnimalTest.java
@@ -0,0 +1,21 @@
+package com.JavaFundamentals.OOPS.features.Abstractions.Task.AnimalClassificationSystem;
+
+public class AnimalTest {
+
+    public static void main(String[] args) {
+
+
+
+        AnimalImp animalImp = new Dog("sai",(byte) 9);
+        animalImp.displayDetails();
+        animalImp.makeSound();
+        animalImp.move();
+
+        AnimalImp animalImp1 = new Bird("ravi",(byte) 5);
+        animalImp1.displayDetails();
+        animalImp1.makeSound();
+        animalImp1.move();
+
+
+    }
+}
