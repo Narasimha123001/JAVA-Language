dfa3b425e8b6bc87d8afcb9fff16be80d4b0f21b
Narasimha <narasimhaanguluri001@gmail.com>
2024-10-01 00:59:36 +0530

    its was a sub class of animal having the iplementaion of abstarct methods according their features

---
+++ JavaFundamentals/OOPS/features/Abstractions/Task/AnimalClassificationSystem/Bird.java
@@ -0,0 +1,18 @@
+package com.JavaFundamentals.OOPS.features.Abstractions.Task.AnimalClassificationSystem;
+
+public class Bird extends AnimalImp{
+
+    public Bird(String name, byte age) {
+        super(name, age);
+    }
+
+    @Override
+    void makeSound() {
+        System.out.println("Bird is making a sound........");
+    }
+
+    @Override
+    void move() {
+        System.out.println("bird was fly on the sky...");
+    }
+}
