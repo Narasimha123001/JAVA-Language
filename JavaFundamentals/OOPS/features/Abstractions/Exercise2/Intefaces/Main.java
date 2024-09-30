dd711ea83b3652f5a30f9b4d61c363133896e2d3
Narasimha <narasimhaanguluri001@gmail.com>
2024-10-01 00:28:18 +0530

    its contains a instance of child class,main functions and implemtation of them

---
+++ JavaFundamentals/OOPS/features/Abstractions/Exercise2/Intefaces/Main.java
@@ -0,0 +1,8 @@
+package com.JavaFundamentals.OOPS.features.Abstractions.Exercise2.Intefaces;
+
+public class Main {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+        rectangle.draw();
+    }
+}
