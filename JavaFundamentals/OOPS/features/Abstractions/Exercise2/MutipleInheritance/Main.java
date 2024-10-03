b8c712f2765beaab486419e94716fc9de657792a
Narasimha <narasimhaanguluri001@gmail.com>
2024-10-03 21:13:18 +0530

    implementation of the sub class according their features

---
+++ JavaFundamentals/OOPS/features/Abstractions/Exercise2/MutipleInheritance/Main.java
@@ -0,0 +1,11 @@
+package com.JavaFundamentals.OOPS.features.Abstractions.Exercise2.MutipleInheritance;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Bat bat = new Bat();
+
+        bat.eat();
+        bat.fly();
+    }
+}
