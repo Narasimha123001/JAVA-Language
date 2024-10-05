c7898f5dc486de5ba90858a14a95a95b2fc226e7
Narasimha <narasimhaanguluri001@gmail.com>
2024-10-05 22:49:56 +0530

    sub class of same package

---
+++ JavaFundamentals/OOPS/features/Encapsulations/p1/C.java
@@ -0,0 +1,11 @@
+package com.JavaFundamentals.OOPS.features.Encapsulations.p1;
+
+public class C extends A {
+    public static void main(String[] args) {
+        A a;
+        a = new A();
+        System.out.println("public variable :"+a.x);
+        //System.out.println("private variable :"+a.y);    not access
+        System.out.println("protected variable :"+a.d);
+    }
+}
