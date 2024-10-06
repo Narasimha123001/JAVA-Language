5385664cfb01cbc6e76d77f2f044d68fa2224b17
Narasimha <narasimhaanguluri001@gmail.com>
2024-10-06 22:35:03 +0530

    Access Package from Another Package

---
+++ JavaFundamentals/OOPS/features/Encapsulations/utilities/MathUtil.java
@@ -0,0 +1,8 @@
+package com.JavaFundamentals.OOPS.features.Encapsulations.utilities;
+//How to Access Package from Another Package?
+public class MathUtil {
+    public static  int add(int a, int b){
+        return  a + b;
+    }
+
+}
