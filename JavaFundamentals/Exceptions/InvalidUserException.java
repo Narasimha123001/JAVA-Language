5fffdffb1386deb75c05aa04cd16ffdc897cb21f
Narasimha <narasimhaanguluri001@gmail.com>
2024-10-08 23:01:45 +0530

    its its sub class that inherts from Exceptions

---
+++ JavaFundamentals/Exceptions/InvalidUserException.java
@@ -0,0 +1,8 @@
+package com.JavaFundamentals.Exceptions;
+
+public class InvalidUserException extends Exception {
+
+    public InvalidUserException(String message) {
+        super(message);
+    }
+}
