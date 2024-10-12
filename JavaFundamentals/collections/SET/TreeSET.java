b2112143d8c6f1c9a059dc7dacce149aaea8578e
Narasimha <narasimhaanguluri001@gmail.com>
2024-10-12 22:05:54 +0530

    implementing the treeset in java

---
+++ JavaFundamentals/collections/SET/TreeSET.java
@@ -0,0 +1,23 @@
+package com.JavaFundamentals.collections.SET;
+
+import java.util.TreeSet;
+
+public class TreeSET {
+    public static void main(String[] args) {
+
+        TreeSet<String> treeSet = new TreeSet();
+        treeSet.add("nani");
+        treeSet.add("shiva");
+        treeSet.add("hari");
+        treeSet.add("apple");
+        treeSet.add("apple");
+
+        for (String str : treeSet){
+            System.out.println(str);
+        }
+    }
+}
+
+        /*
+        TreeSet: its is order doesn't allow the duplicates
+         */
