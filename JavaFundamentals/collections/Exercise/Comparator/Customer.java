b6a9b9b39f2539071bd8d7bf72406c4fc7efe26c
Narasimha <narasimhaanguluri001@gmail.com>
2024-11-03 19:11:23 +0530

    its was class having the private feilds , override methods and Comparator methods

---
+++ JavaFundamentals/collections/Exercise/Comparator/Customer.java
@@ -0,0 +1,46 @@
+package com.JavaFundamentals.collections.Exercise.Comparator;
+
+import java.util.Comparator;
+
+public class Customer implements Comparator<Customer> {
+    
+    private int id;
+    private String name;
+
+    public Customer() {
+    }
+
+    public Customer(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int compare(Customer c1, Customer c2) {
+        return c1.getName().compareTo(c2.getName());
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
