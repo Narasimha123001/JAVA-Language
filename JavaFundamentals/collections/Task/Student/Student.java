b0835370442cd72c72b43fac7e84439e357d5090
Narasimha <narasimhaanguluri001@gmail.com>
2024-12-04 22:53:31 +0530

    a bean file with no-arg constructor, getter and setter some override functions

---
+++ JavaFundamentals/collections/Task/Student/Student.java
@@ -0,0 +1,50 @@
+package com.JavaFundamentals.collections.Task.Student;
+
+import java.util.Objects;
+
+public class Student {
+    private int studentId;
+    private String studentName;
+
+    public Student(int studentId, String studentName) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentId=" + studentId +
+                ", studentName='" + studentName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return studentId == student.studentId && Objects.equals(studentName, student.studentName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(studentId, studentName);
+    }
+}
