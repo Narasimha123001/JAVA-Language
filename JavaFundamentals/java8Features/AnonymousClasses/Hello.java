e8d30999c2d62a3eb74cda7e2e5f30c8f9873970
Narasimha <narasimhaanguluri001@gmail.com>
2024-12-05 23:08:50 +0530

    functional interface

---
+++ JavaFundamentals/java8Features/AnonymousClasses/Hello.java
@@ -0,0 +1,8 @@
+package com.JavaFundamentals.java8Features.AnonymousClasses;
+
+//Functional Interface are used for simplifying anonymous inner class especially in functional programing
+@FunctionalInterface
+public interface Hello {
+
+    void say(String helloHi);
+}
